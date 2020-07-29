de4d60b0fb3026fc54c901925da567b272e5d34e
trankimthuong <thuongtk.bk@gmail.com>
2020-07-29 08:09:38 +0700

    Tim phan tu lon nhat trong mang

---
+++ src/FindMaxElement.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class FindMaxElement {
+    public static void main(String[] args) {
+        int size;
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.print("Enter a size: ");
+            size = scanner.nextInt();
+            if(size > 20){
+                System.out.println("Size should not exceed 20!!!");
+            }
+        }while (size > 20);
+
+        int[] array = new int[size];
+        int i = 0;
+        while(i < size){
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+
+        System.out.println("Property list: ");
+        for(int j = 0; j < array.length; j++){
+            System.out.print(array[j] + "\t");
+        }
+
+        int max = array[0];
+        int index = 1;
+        for(int j = 1; j < array.length; j++){
+            if(array[j] > max){
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max +
+                ", at position " + index +"!!!");
+    }
+}
